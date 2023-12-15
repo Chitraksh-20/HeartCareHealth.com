27c44e5f78f274779531c86761011d456121ae23
AyushTripathiIITP <ayush.tripathiiitp@gmail.com>
2023-12-15 12:39:34 +0530

    working till now

---
+++ app.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+# Create flask app
+flask_app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+
+@flask_app.route("/")
+def Home():
+    return render_template("index1.html")
+
+@flask_app.route("/login")
+def Login():
+    return render_template("index.html")
+
+@flask_app.route("/predict", methods = ["POST"])
+def predict():
+    
+    params = ['Age' , 'Sex' , 'Chest Pain Type' , 'Resting Blood Pressure' , 'Serum Cholestrol' , 'Fasting Blood Sugar' , 'Resting Electrocardiographic Results' , 'Maximum Heart Rate Achieved' , 'Exercise Induced Angina' , 'ST Depression Induced by Exercise Relative to Rest' , 'The Slope of The Peak Exercise ST Segment' , 'Number of Major Vessels Colored by Flourosopy' , 'Thallium Stress Test']
+    float_features = []
+    
+    for param in params:
+        float_features.append(float(request.form.get(param)))
+        
+        
+    # float_features = [float(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    features = pd.DataFrame(features , columns = ['Age' , 'Sex' , 'Chest Pain Type' , 'Resting Blood Pressure' , 'Serum Cholestrol' , 'Fasting Blood Sugar' , 'Resting Electrocardiographic Results' , 'Maximum Heart Rate Achieved' , 'Exercise Induced Angina' , 'ST Depression Induced by Exercise Relative to Rest' , 'The Slope of The Peak Exercise ST Segment' , 'Number of Major Vessels Colored by Flourosopy' , 'Thallium Stress Test'])
+    prediction = model.predict(features)
+    if(prediction == 1):
+        return render_template('index1.html', prediction_text = "You should consult a Cardiologist at the earliest.")
+    else:
+        return render_template('index1.html', prediction_text = "You are Fit Health-wise")
+
+
+if __name__ == "__main__":
+    flask_app.run(debug=True)
